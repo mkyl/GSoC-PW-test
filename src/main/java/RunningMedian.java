96d878f380b975b775c385a306a6daed6d4a62b2
Kayali <me@kayali.io>
2017-03-27 00:22:58 -0700

    Fixed odd number of elements bug

--- src/main/java/RunningMedian.java
+++ src/main/java/RunningMedian.java
@@ -10,15 +10,25 @@ public class RunningMedian {
         Scanner inputScanner = new Scanner(input);
         int elementCount = inputScanner.nextInt();
 
-        for(int i = 1; i <= elementCount / 2; i++) {
+        if(elementCount < 1) {
+            throw new IllegalArgumentException("Number of elements must be 1 or more");
+        }
+
+        for(int i = 1; i <= elementCount / 2 + 1; i++) {
             integerList.add(inputScanner.nextInt());
-            double firstElement = integerList.remove();
-            System.out.printf("%.2f%n", firstElement);
-            
-            integerList.add(inputScanner.nextInt());
+        }
+
+        double firstElement = integerList.remove();
+        System.out.printf("%.2f%n", firstElement);
+
+        while(!integerList.isEmpty()) {
             int secondElement = integerList.element();
             double average = (firstElement + secondElement) / 2.0;
             System.out.printf("%.2f%n", average);
+
+            firstElement = integerList.remove();
+            if (!integerList.isEmpty() || elementCount % 2 == 1)
+                System.out.printf("%.2f%n", firstElement);
         }
     }
 }
