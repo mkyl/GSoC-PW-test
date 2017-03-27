9bfdd0b1b1cdabd3d699b189f7c29d2e3d110979
Kayali <me@kayali.io>
2017-03-26 19:45:27 -0700

    Implemented running median with queue

--- src/main/java/RunningMedian.java
+++ src/main/java/RunningMedian.java
@@ -1,12 +1,24 @@
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RunningMedian {
-    public boolean someLibraryMethod() {
-        return false;
-    }
+    Queue<Integer> integerList = new LinkedList<Integer>();
 
-    public void process(InputStream input) {
+    public void process(String input) {
         Scanner inputScanner = new Scanner(input);
+        int elementCount = inputScanner.nextInt();
+
+        for(int i = 1; i <= elementCount / 2; i++) {
+            integerList.add(inputScanner.nextInt());
+            double firstElement = integerList.remove();
+            System.out.printf("%.2f%n", firstElement);
+            
+            integerList.add(inputScanner.nextInt());
+            int secondElement = integerList.element();
+            double average = (firstElement + secondElement) / 2.0;
+            System.out.printf("%.2f%n", average);
+        }
     }
 }
